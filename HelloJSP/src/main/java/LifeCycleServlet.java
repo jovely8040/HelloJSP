c2ec8fdd6a45346afedc5e0acef27dc85ab6e788
조은혜 <jovely8040@gmail.com>
2021-08-12 15:41:33 +0900

    초기화 파라미터

--- HelloJSP/src/main/java/LifeCycleServlet.java
+++ HelloJSP/src/main/java/LifeCycleServlet.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +11,14 @@ public class LifeCycleServlet extends HttpServlet {
 	@Override
 	public void init() throws ServletException {
 		System.out.println("init() 메서드 호출");
+		
+		ServletContext context = getServletContext();
+		// 컨텍스트 파라미터 받아오기
+		String dbuser = context.getInitParameter("dbuser");
+		String dbpass = context.getInitParameter("dbpass");
+		
+		System.out.println("컨켁스트 파라미터 dbuser:" + dbuser);
+		System.out.println("컨켁스트 파라미터 dbpass:" + dbpass);
 	}
 
 	@Override
@@ -21,6 +31,11 @@ public class LifeCycleServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("doGet() 메서드 호출");
+		
+		// 서블릿 초기화 파라미터 받아오기
+		ServletConfig config = getServletConfig();
+		String servletParam = config.getInitParameter("servlet-param");
+		System.out.println("서블릿 초기화 파라미터:" + servletParam);
 	}
 
 	@Override
