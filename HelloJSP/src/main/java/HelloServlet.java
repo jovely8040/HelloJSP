32c98d075fa38f36b8c82bdb0935c32a6b09bd53
조은혜 <jovely8040@gmail.com>
2021-08-12 14:25:38 +0900

    POST 전송

--- HelloJSP/src/main/java/HelloServlet.java
+++ HelloJSP/src/main/java/HelloServlet.java
@@ -12,6 +12,10 @@ public class HelloServlet extends HttpServlet {
 	// Get방식의 요청을 처리 -> doGet()
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		// 응답의 페이지 타입을 결정
+		resp.setContentType("text/html; charset=UTF-8");
+		
 		//  파라미터 확인
 		String name = req.getParameter("name");
 		if (name == null) {
@@ -23,4 +27,20 @@ public class HelloServlet extends HttpServlet {
 		out.println("<p>Welcome, " + name + "</p>");
 	}
 
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 페이지 응답 설정
+		resp.setContentType("text.html; charset=UTF-8");
+		
+		//폼 데이터 받기(파라미터 받기)
+		String firstName = req.getParameter("first_name");
+		String lastName = req.getParameter("last_name");
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<h3>Form Data</h3>");
+		out.println("<p>성:" + lastName + "</p>");
+		out.println("<p>이름:" + firstName + "</p>");
+	}
+	
+	
 }
