5501e7f885ce776c2ecb3471c29438ae11f75765
조은혜 <jovely8040@gmail.com>
2021-08-13 13:51:08 +0900

    Emaillist (Model 1)

---
+++ HelloJSP/src/main/java/com/example/emaillist/vo/EmailVo.java
@@ -0,0 +1,51 @@
+package com.example.emaillist.vo;
+
+import java.util.Date;
+
+public class EmailVo {
+	// 필드 (Long 랩퍼타입을 써라~)
+	private Long no;
+	private String lastName;
+	private String firstName;
+	private String email;
+	private Date createAt;
+	
+	// Getter/Setter
+	public Long getNo() {
+		return no;
+	}
+	public void setNo(Long no) {
+		this.no = no;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getCreateAt() {
+		return createAt;
+	}
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+	
+	@Override
+	public String toString() {
+		return "EmailVo [no=" + no + ", lastName=" + lastName + ", firstName=" + firstName + ", email=" + email
+				+ ", createAt=" + createAt + "]";
+	}
+	
+}
